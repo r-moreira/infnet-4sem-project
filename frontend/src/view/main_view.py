af8ac77cbc3eabba081a0131092e1053849f3c7d
Rodrigo Avila <rodrigo@Rodrigos-MacBook-Pro-3.local>
2024-10-20 18:46:07 -0300

    global error handler

--- frontend/src/view/main_view.py
+++ frontend/src/view/main_view.py
@@ -4,8 +4,12 @@ from view.abstract_view import AbstractView
 from utils.streamlit_utils import StreamlitUtils
 from view.abstract_view import AbstractView
 from view.abstract_strategy_view import AbstractStrategyView
+import streamlit as st
+import logging
 
 class MainView(AbstractView):
+    logger = logging.getLogger(__name__)
+    
     def __init__(
         self, 
         sidebar_view: AbstractView,
@@ -15,7 +19,14 @@ class MainView(AbstractView):
         self._sidebar_view = sidebar_view
         self._strategy_view_list = strategy_view_list
         
-    def show(self) -> None:
+    def show(self) -> None:        
+        try:
+            self.do_show()
+        except Exception as e:
+            MainView.logger.error(f"Error rendering View: {e}")
+            self.global_error_dialog()
+       
+    def do_show(self) -> None:
         StreamlitUtils.setup_page_config()
         
         # Strategy pattern
@@ -25,3 +36,7 @@ class MainView(AbstractView):
             if strategy_view.accept(strategy):
                 strategy_view.show()
                 break
+       
+    @st.dialog("Error")
+    def global_error_dialog(self) -> None:
+        st.error(f"Something went wrong :(")
