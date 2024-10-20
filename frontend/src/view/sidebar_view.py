e0f66dbe8e32888c242cb72f1a836b7780b329f0
Rodrigo Avila <rodrigo@Rodrigos-MacBook-Pro-3.local>
2024-10-20 12:19:42 -0300

    option menu

--- frontend/src/view/sidebar_view.py
+++ frontend/src/view/sidebar_view.py
@@ -1,19 +1,35 @@
 import streamlit as st
 from view.abstract_view import AbstractView
 from enums.view_strategy import ViewStrategy
-from typing import Literal
+from streamlit_option_menu import option_menu 
+from streamlit_extras.add_vertical_space import add_vertical_space
 
 class SidebarView(AbstractView):
     
     @staticmethod
     def show() -> ViewStrategy:
-        st.sidebar.title("🤖 Deep Listen 🎸")
-        st.sidebar.markdown("---")
-        st.sidebar.markdown("This app is a chatbot that uses OpenAI's GPT-4 model to generate responses to analyze and discuss music.")
-        st.sidebar.markdown("The chat is in beta, it works, but like a normal Chat GPT chat, music features will be added soon.")
-        st.sidebar.markdown("---")
-        
-        strategy: str = st.sidebar.radio("Select APP Mode:", options=ViewStrategy.to_value_list(), index=0)
-        
+        with st.sidebar:
+            add_vertical_space(2)
+            
+            st.title("🤖 Deep Listen 🎸")
+            
+            add_vertical_space(4)
+            
+            strategy = option_menu(
+                "Main Menu", 
+                ViewStrategy.to_value_list(), 
+                icons=[
+                    'music-note-list',
+                    'chat'
+                ],
+                menu_icon="cast", 
+                default_index=0
+            )
+            
+            st.divider()
+            
+            st.markdown("This app is a chatbot that uses OpenAI's GPT-4 model to generate responses to analyze and discuss music.")
+            st.markdown("The chat is in beta, it works, but like a normal Chat GPT chat, music features will be added soon.")
+           
         return ViewStrategy(strategy)
        
